c135bb8f14276b2e456e9d296455f903f836b300
Athan Clark <athan.clark@localcooking.com>
2018-10-30 07:04:07 -0600

    more code

--- src/Web/Dependencies/BlueJay/Types.hs
+++ src/Web/Dependencies/BlueJay/Types.hs
@@ -1,12 +1,16 @@
 {-# LANGUAGE
     GeneralizedNewtypeDeriving
   , DeriveGeneric
+  , OverloadedStrings
   #-}
 
 module Web.Dependencies.BlueJay.Types where
 
-import Data.Text (Text)
+import Data.Text (Text, intercalate, unpack)
 import Data.Hashable (Hashable)
+import Data.Aeson (ToJSON (..), FromJSON (..), Value (String))
+import Data.Aeson.Attoparsec (attoAeson)
+import Data.Attoparsec.Text (Parser, char, sepBy, takeWhile1)
 import Control.DeepSeq (NFData)
 import GHC.Generics (Generic)
 
@@ -22,3 +26,18 @@ type Client m input output = (input -> m (Maybe output)) -> m ()
 newtype Topic = Topic
   { getTopic :: [Text]
   } deriving (Eq, Ord, Generic, Hashable, NFData)
+
+
+instance Show Topic where
+  show (Topic x) = unpack (intercalate "/" x)
+
+instance FromJSON Topic where
+  parseJSON = attoAeson (Topic <$> breaker)
+    where
+      breaker :: Parser [Text]
+      breaker = takeWhile1 (/= '/') `sepBy` char '/'
+
+instance ToJSON Topic where
+  toJSON (Topic xs) = String (intercalate "/" xs)
+
+
